5cd48f2b372a09827248126929a7aef76bdd6637
David3Emmanuel <david3emmanuel@gmail.com>
2026-01-01 17:57:13 +0100

    Integrate Google Analytics

--- app/root.tsx
+++ app/root.tsx
@@ -40,6 +40,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
+
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-CLYBGGRKW9'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CLYBGGRKW9');
+          `,
+          }}
+        />
       </head>
       <body>
         {children}
